8b92300f1f9e393fd206e7c9fab45d1155947112
João Pedro Caldeira <jpc130@gmail.com>
2019-10-25 17:13:03 +0100

    missing the bonus iteraction

--- starter_code/src/movies.js
+++ starter_code/src/movies.js
@@ -32,29 +32,34 @@ function calculateAverageDramaRate(movies) {
 // Iteration 3: Ordering by duration - Order by time duration, ascending (in growing order)
 
 
-const sortedMovies = [...MOVIES]
 
 
-function orderByYear(sortedMovies) {
-    return sortedMovies.sort((a, b) => {
+
+function orderByYear(movies) {
+    const sortedYear = [...movies]
+    return sortedYear.sort((a, b) => {
         if (a.year > b.year) {
-            return 1
+            return 1;
         } else if (a.year < b.year) {
-            return -1
+            return -1;
         } else {
-            if(a.title> b.title){
-                return 1
-            }else if(a.title< b.title){
-                return -1
+            if (a.title > b.title) {
+                return 1;
+            } else if (a.title < b.title) {
+                return -1;
             }
-                
+
         }
     })
 }
 
+
+
+
+
 // Iteration 4: Steven Spielberg. The best? - How many movies did STEVEN SPIELBERG direct
-let spielbergDrama=[];
-function countSpielbergDramaMovies(movies){
+let spielbergDrama = [];
+function countSpielbergDramaMovies(movies) {
     spielbergDrama = movies.filter((value) => {
         return value.genre.includes("Drama") && value.director.includes('Steven Spielberg')
     }); if (spielbergDrama.length === 0) {
@@ -64,14 +69,49 @@ function countSpielbergDramaMovies(movies){
     }
 }
 
+
+
+
+
 // Iteration 5: Alphabetic Order - Order by title and print the first 20 titles
-
-let moviesAlphabetically = [ ...MOVIES]
-function orderAlphabetically(movies){
-    
-
+function orderAlphabetically(movies) {
+    let sortedMovies = [...movies]
+    sortedMovies.sort((a, b) => {
+        if (a.title.toLowerCase() > b.title.toLowerCase()) {
+            return 1
+        } else if (a.title.toLowerCase() < b.title.toLowerCase()) {
+            return -1
+        } else {
+            return 0
+        }
+    });
+    let titlesList = sortedMovies.map((value) => value.title)
+    return titlesList.slice(0, 20);
 }
 
-// Iteration 6: Time Format - Turn duration of the movies from hours to minutes
 
-// BONUS Iteration: Best yearly rate average - Best yearly rate average
+
+// Iteration 6: Time Format - Turn duration of the movies from hours to minutes
+function turnHoursToMinutes(movies) {
+    const timeList = movies.map((value) => { return { duration: value.duration.split(" ") } });
+    const timeInMin = timeList.map((value) => {
+        let sum = 0;
+        /* console.log('value', value) */
+        for (i = 0; i < value.duration.length; i++) {
+
+            if (value.duration[i].includes("h")) {
+                sum += parseInt(value.duration[i]) * 60;
+              /*   console.log('value', value.duration[i])
+                console.log(`adding hours in minutes-sum`, sum) */
+            } else if (value.duration[i].includes("m")) {
+                sum += parseInt(value.duration[i]);
+               /*  console.log('value', value.duration[i])
+                console.log(`adding minutes to hours-sum`, sum) */
+            }
+        }
+        return { duration: sum };
+    });
+    return timeInMin;
+}
+
+
